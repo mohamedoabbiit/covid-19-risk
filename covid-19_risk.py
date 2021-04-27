63389fc2fd6fca1403bf305acea44e8b214057d0
mohammedoabbiit <mohamedoabbiit@gmail.com>
2021-04-27 18:08:33 -0400

    upload the Covid task file to my reposit

---
+++ covid-19_risk.py
@@ -0,0 +1,27 @@
+# age = input("Are you a cigarette addict older than 75 years old? 'Yes' or 'No': ")
+# chronic=input("Do you have a severe chronic disease? 'Yes' or 'No': ")
+# immune = input("Is your immune system too weak? 'Yes' or 'No': ")
+# if (age =='Yes' and chronic =='Yes') or (age =='Yes' and immune =='Yes'):
+    
+#     print ("You are in risky group")
+# else:
+#     print("You are not in risky group")  
+# 
+# a = 1
+#  # This will print 'yes'
+# if a is 1:
+#     print('yes')
+
+# b = 1000
+# # This won't!
+# if b is 1000:
+#     print('yes')      
+age = input("Are you a cigarette addict older than 75 years old? 'Yes' or 'No': ")
+chronic=input("Do you have a severe chronic disease? 'Yes' or 'No': ")
+immune = input("Is your immune system too weak? 'Yes' or 'No': ")
+
+if (age =='Yes' and chronic =='Yes') or (age =='Yes' and immune =='Yes'):
+    
+    print ("You are in risky group")
+else:
+    print("You are not in risky group")
